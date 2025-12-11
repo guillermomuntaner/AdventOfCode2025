ac7df836066ea3a5ee7648a1580cb1bbab6269b9
Guillermo Muntaner <guillermomp87@gmail.com>
2025-12-11 09:16:33 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,84 @@
+import kotlin.math.abs
+
 fun main() {
+    fun parseInput(input: List<String>): List<Int> = input
+        .map { line ->
+            line.substring(1).toInt().let {
+                if(line[0] == 'L') {
+                    -it
+                } else {
+                    it
+                }
+            }
+        }
+
+    checkEquals(50, 50.mod(100))
+    checkEquals(0, 0.mod(100))
+    checkEquals(99, (-1).mod(100))
+    checkEquals(0, 100.mod(100))
+    checkEquals(0, (-100).mod(100))
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return parseInput(input).fold(50 to 0) { (acc, count), instruction ->
+            var count = count
+            val newAcc = (acc + instruction).mod (100)
+            if (newAcc == 0) count += 1
+            newAcc to count
+        }.second
     }
+
+    fun Int.countWraps(range: Int): Int =
+        abs(this.floorDiv(range))
+
+    checkEquals(0, 0.countWraps(100))
+    checkEquals(0, 1.countWraps(100))
+    checkEquals(0, 4.countWraps(100))
+    checkEquals(0, 99.countWraps(100))
+    checkEquals(1, 100.countWraps(100))
+    checkEquals(1, 150.countWraps(100))
+    checkEquals(2, 250.countWraps(100))
+    checkEquals(1, (-1).countWraps(100))
+    checkEquals(1, (-100).countWraps(100))
+    checkEquals(2, (-101).countWraps(100))
+    checkEquals(3, (-201).countWraps(100))
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return parseInput(input).fold(50 to 0) { (acc, count), instruction ->
+            var count = count
+            instruction.println()
+            val newAcc = (acc + instruction)
+            count += newAcc.countWraps(100)
+            if (acc == 0 && newAcc < 0) {
+                count -= 1
+            }
+            val modAcc = newAcc.mod(100)
+            if (modAcc == 0 && newAcc <= 0) {
+                count += 1
+            }
+            modAcc.println()
+            count.println()
+            modAcc to count
+        }.second
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    checkEquals(3, part1(testInput))
+
+    checkEquals(1, part2(listOf("R60")))
+    checkEquals(1, part2(listOf("L60")))
+    checkEquals(1, part2(listOf("R50")))
+    checkEquals(1, part2(listOf("R49", "R1")))
+    checkEquals(1, part2(listOf("L50")))
+    checkEquals(1, part2(listOf("L51")))
+    checkEquals(1, part2(listOf("L50", "L1")))
+    checkEquals(1, part2(listOf("L49", "L1")))
+    checkEquals(1, part2(listOf("L49", "L1", "L1")))
+    checkEquals(2, part2(listOf("L50", "L100")))
+    checkEquals(3, part2(listOf("L50", "L200")))
+    checkEquals(6, part2(testInput))
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    checkEquals(980, part1(input))
+    checkEquals(5961, part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -2,6 +2,7 @@ import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readText
+import kotlin.math.abs
 
 /**
  * Reads lines from the given input txt file.
@@ -19,3 +20,31 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun <T, E>checkEquals(expected: T, actual: E) {
+    if (expected != actual) {
+        val message = "Check failed. Expected $expected, got $actual"
+        throw IllegalStateException(message)
+    }
+}
+
+fun <T>Any?.checkEqualTo(expected: T) = checkEquals(expected = expected, actual = this)
+
+fun <T> List<T>.toPair(): Pair<T, T> {
+    require(size == 2)
+    return Pair(this[0], this[1])
+}
+
+fun <T> MutableList<T>.swap(index1: Int, index2: Int) {
+    val temp = this[index1]
+    this[index1] = this[index2]
+    this[index2] = temp
+}
+
+fun gcd(a: Int, b: Int): Int {
+    return if (b == 0) a else gcd(b, a % b)
+}
+
+fun lcm(a: Int, b: Int): Int {
+    return abs(a * b) / gcd(a, b)
+}
